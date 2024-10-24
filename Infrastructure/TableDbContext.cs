3dc7052b3247cfc3b73e5699d11505ff499c922d
Grinik767 <grigorikiseleff@yandex.ru>
2024-10-24 17:27:30 +0500

    add db contexts

---
+++ Infrastructure/TableDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure;
+
+public class TableDbContext(IConfiguration configuration) : DbContext
+{
+    public DbSet<Table> Tables => Set<Table>();
+    public DbSet<TableGroup> TableGroups => Set<TableGroup>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString("Database"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TableGroup>()
+            .HasKey(tg => new { tg.TableId, tg.GroupId });
+
+        modelBuilder.Entity<TableGroup>()
+            .HasOne(tg => tg.Table)
+            .WithMany(t => t.TableGroups)
+            .HasForeignKey(tg => tg.TableId);
+    }
+}
