a319106654f2e7b273c0c26334aa58ff63c6191e
Grinik767 <grigorikiseleff@yandex.ru>
2024-10-24 17:09:17 +0500

    add Domain

---
+++ Domain/Entities/Group.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities;
+
+
+public class Group(string title)
+{
+    public Guid Id { get; private set; }
+    public string Title { get; private set; } = title;
+    public ICollection<TableGroup> TableGroups { get; private set; } = new List<TableGroup>();
+
+    public void UpdateTitle(string title) => Title = title;
+}
