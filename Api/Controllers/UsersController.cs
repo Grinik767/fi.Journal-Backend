cbfb804b742c7de00e6526d69dd7eca122be5a73
Grinik767 <grigorikiseleff@yandex.ru>
2024-10-27 15:01:11 +0500

    users get by id

--- Api/Controllers/UsersController.cs
+++ Api/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Api.Dtos;
 using Domain.Entities;
 using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers;
 
@@ -19,5 +20,18 @@ public class UsersController(JournalDbContext dbContext) : ControllerBase
         await dbContext.SaveChangesAsync(ct);
 
         return Ok(new UserDto(user.Id, user.Name, user.Email));
+    }
+
+    [HttpGet]
+    [Route("{guid:guid}")]
+    public async Task<IActionResult> Get(Guid guid, CancellationToken ct)
+    {
+        var user = await dbContext.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(user => user.Id == guid, cancellationToken: ct);
+
+        return user is null
+            ? NotFound()
+            : Ok(new UserDto(user.Id, user.Name, user.Email));
     }
 }
