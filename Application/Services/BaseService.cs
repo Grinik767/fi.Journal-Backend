3ac102572c982f6523c34ca5c0401388d9f610e3
Grinik767 <grigorikiseleff@yandex.ru>
2024-12-23 16:47:09 +0500

    start exceptions fix

--- Application/Services/BaseService.cs
+++ Application/Services/BaseService.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Extensions;
 using Domain.Entities;
 using FluentValidation;
+using Infrastructure.Exceptions;
 using Infrastructure.Repositories;
 
 namespace Application.Services;
@@ -8,6 +9,22 @@ namespace Application.Services;
 public class BaseService<T>(IRepository<T> repository, IValidator<T> validator)
     where T : Entity<Guid>
 {
+    public async Task Delete(Guid id, CancellationToken ct) => await repository.Delete(id, ct);
+
+    public async Task<List<T>> GetAll(CancellationToken ct) => await repository.GetAll(ct);
+
+    public async Task<T> GetById(Guid id, CancellationToken ct)
+    {
+        try
+        {
+            return await repository.GetById(id, ct);
+        }
+        catch (InvalidOperationException e)
+        {
+            throw new EntityNotFoundException("Object not found", e);
+        }
+    }
+    
     protected async Task<T> Add(T entity, CancellationToken ct)
     {
         await entity.ValidateAsync(validator, ct);
@@ -21,10 +38,4 @@ public class BaseService<T>(IRepository<T> repository, IValidator<T> validator)
         await entity.ValidateAsync(validator, ct);
         return await repository.Update(entity, ct);
     }
-
-    public async Task Delete(Guid id, CancellationToken ct) => await repository.Delete(id, ct);
-
-    public async Task<List<T>> GetAll(CancellationToken ct) => await repository.GetAll(ct);
-
-    public async Task<T> GetById(Guid id, CancellationToken ct) => await repository.GetById(id, ct);
 }
