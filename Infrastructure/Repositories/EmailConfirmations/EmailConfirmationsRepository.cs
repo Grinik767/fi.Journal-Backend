209e59a562a79168716cdf587f6bc7486c520023
Grinik767 <grigorikiseleff@yandex.ru>
2025-01-09 21:41:39 +0500

    finish repository

--- Infrastructure/Repositories/EmailConfirmations/EmailConfirmationsRepository.cs
+++ Infrastructure/Repositories/EmailConfirmations/EmailConfirmationsRepository.cs
@@ -1,31 +1,33 @@
 ﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories.EmailConfirmations;
 
-public class EmailConfirmationsRepository : IEmailConfirmationsRepository
+public class EmailConfirmationsRepository(JournalDbContext dbContext) : IEmailConfirmationsRepository
 {
-    public Task Add(EmailConfirmation entity, CancellationToken ct)
+    public async Task Add(EmailConfirmation confirmation, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        await dbContext.EmailConfirmations.AddAsync(confirmation, ct);
+        await dbContext.SaveChangesAsync(ct);
     }
 
-    public Task<EmailConfirmation> Update(EmailConfirmation entity, CancellationToken ct)
+    public async Task<EmailConfirmation> Update(EmailConfirmation confirmation, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        await dbContext.SaveChangesAsync(ct);
+        return confirmation;
     }
 
-    public Task Delete(Guid id, CancellationToken ct)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task Delete(Guid id, CancellationToken ct) =>
+        await dbContext.EmailConfirmations
+            .Where(u => u.Id == id)
+            .ExecuteDeleteAsync(ct);
 
-    public Task<EmailConfirmation> GetById(Guid id, CancellationToken ct)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<EmailConfirmation> GetById(Guid id, CancellationToken ct) =>
+        await dbContext.EmailConfirmations
+            .Include(e => e.User)
+            .FirstAsync(e => e.Id == id, ct);
 
-    public Task<List<EmailConfirmation>> GetAll(CancellationToken ct)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<List<EmailConfirmation>> GetAll(CancellationToken ct) =>
+        await dbContext.EmailConfirmations.AsNoTracking()
+            .ToListAsync(ct);
 }
