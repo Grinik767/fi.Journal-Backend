0b28ee445a533299d380fa38e332872cc0b4d504
Grinik767 <grigorikiseleff@yandex.ru>
2025-01-10 11:49:17 +0500

    mailer added

---
+++ Mailer/EmailService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.Extensions.Options;
+
+namespace Mailer;
+
+public class EmailService
+{
+    private readonly SmtpClient _smtpClient;
+
+    public EmailService(IOptions<MailerOptions> mailerOptions)
+    {
+        var options = mailerOptions.Value;
+
+        using var smtpClient = new SmtpClient(options.Host, options.Port);
+        smtpClient.Credentials = new NetworkCredential(options.Email, options.Password);
+        smtpClient.EnableSsl = true;
+
+        _smtpClient = smtpClient;
+    }
+
+    public async Task Send(MailMessage mailMessage)
+    {
+        try
+        {
+            await _smtpClient.SendMailAsync(mailMessage);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error while sending email: {ex.Message}");
+            throw;
+        }
+    }
+}
---
+++ Mailer/MailerOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Mailer;
+
+public class MailerOptions
+{
+    public string Host { get; init; } = string.Empty;
+    public int Port { get; init; }
+    public string Email { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+}
